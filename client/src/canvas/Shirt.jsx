04a352f6d8f2494ae1d26af2b4806bca4e5e58b6
Sakibur-Karim <sakiburkarim@gmail.com>
2023-08-13 12:35:19 -0400

    update Shirt.jsx

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -41,7 +41,6 @@ const Shirt = () => {
             rotation={[0, 0, 0]}
             scale={0.15}
             map={logoTexture}
-            map-anisotropy={16}
             depthTest={false}
             depthWrite={true}
           />
