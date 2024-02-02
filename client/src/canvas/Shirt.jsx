ec4ad7795973c9ae6e63296a57016100aa718e80
Alejandro Sanchez <133040928+AxelothLeohryn@users.noreply.github.com>
2024-02-02 23:53:22 +0100

    fix/ mapAnisotropy

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -41,7 +41,7 @@ const Shirt = () => {
             rotation={[0, 0, 0]}
             scale={0.15}
             map={logoTexture}
-            map-anisotropy={16}
+            mapAnisotropy={16}
             depthTest={false}
             depthWrite={true}
           />
